8c8420dc12a78d182ea879fbd4d6f72188e937d3
MasterDigimon <gamerdigimonxl@gmail.com>
2026-06-15 11:15:43 -0600

    create Shelf.cs

---
+++ Week-1/librarykata/Library.Domain/Shelf.cs
@@ -0,0 +1,40 @@
+// For demo sake, lets write a generic
+// I want to create a shelf, and a shelf can hold anything
+// I don't want to be limited to LibraryItems, I can put like commputer hardware or supplies on the shelf
+namespace Library.Domain;
+
+// T is the standard placeholder pof ... "some type" that we will determinate later
+// You will see it all over the place in documentation and code examples
+public class Shelf<T>
+{
+    private readonly T[] _slots;
+    private int used; // As things are added to my array, the Shelf object tracks how
+                      // slots of the sheld are being used internally here.
+
+    public Shelf(int capacity)
+    {
+        _slots = new T[capacity];
+    }
+
+    // Exposing some arrray properties as needed
+    public int Capacity => _slots.Length;
+    public int Count => used; // Exposing that use as public property
+
+    // Method to add items to our shelf
+    public bool TryAdd(T item)
+    {
+        if (used == _slots.Length) return false;
+
+        // If the shelf isn't full then...
+        // Access the _slots array's index of the current used + 1
+        // increment used
+        // set that index equal to the incoming item
+        _slots[used++] = item;
+        return true;
+    }
+
+    public T Get(int index) {
+        return _slots[index];
+    }
+
+}
