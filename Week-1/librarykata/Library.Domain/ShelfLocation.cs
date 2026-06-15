c40ddf5c4b5f458437950b4cfddfd20ee7f63ea9
MasterDigimon <gamerdigimonxl@gmail.com>
2026-06-15 10:48:56 -0600

    ShelfLocation.cs

---
+++ Week-1/librarykata/Library.Domain/ShelfLocation.cs
@@ -0,0 +1,23 @@
+namespace Library.Domain;
+
+// Structs are for small bundles of data with no identity.
+// They look kind of like classes but they are VALUE types
+// Meaning - two structs of the same type with the same data are identical
+// If I compare those two structs with .equals() Iget True
+
+public readonly struct ShelfLocation
+{
+    public int Aisle {get;}
+    public int Shelf{get;}
+
+    public ShelfLocation(int aisle, int shelf)
+    {
+        Aisle = aisle;
+        Shelf = shelf;
+    }
+
+    public override string ToString()
+    {
+        return $"Aisle {Aisle}, Shelf {Shelf}";
+    }
+}
