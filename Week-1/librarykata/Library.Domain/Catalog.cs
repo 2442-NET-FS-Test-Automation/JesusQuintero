b67286319c76603fe0dfb69e02f9f491c9c9dd31
MasterDigimon <gamerdigimonxl@gmail.com>
2026-06-15 10:59:14 -0600

    Create Stack, Queue and LinkedList on Catalog.cs

--- Week-1/librarykata/Library.Domain/Catalog.cs
+++ Week-1/librarykata/Library.Domain/Catalog.cs
@@ -15,4 +15,16 @@ public class Catalog
     // with our own wrapper methods and make them internal, private, protected, etc.
     public int Count => _items.Count();
 
+    // Stack<T>: Last in first out - We will modelt a return cart. The most recently returned an item
+    // is re-shelved first.
+    // Primary methods - Push(): puts an item at the top of the Stack, Pop(): removes the top most item
+    public readonly Stack<LibraryItem> _returnCart = new();
+
+    // Queue<T>: First in first out: modeling a hold queue, customers placing holds on books
+    // Primary methods - Enqueue(): join the back of the line, Dequeue(): removed from the front of the line.
+    public readonly Queue<string> _holdQueue = new();
+
+    // Reading list
+    // LinkedList<T>: cheap inserts/removals anywhere in my list, but NO index access.
+    public readonly LinkedList<LibraryItem> _readingList = new();
 }
