3062774922539d8b7b177980c397d238a0f392a3
MasterDigimon <gamerdigimonxl@gmail.com>
2026-06-16 13:18:18 -0600

    LibraryItemFactory.cs

---
+++ Week-1/librarykata/Library.Domain/LibraryItemFactory.cs
@@ -0,0 +1,40 @@
+using System.Runtime.CompilerServices;
+
+namespace Library.Domain;
+
+
+// This factory class is static. It can ONLY contain static members.
+// It CANNOT be instantiated
+// It CANNOT be inherited from
+public static class LibraryItemFactory
+{
+    // Our class is responsible dor creating LibraryItems of any type
+    // we will use that enum here to make sure users ONLY attemp to create
+    // valid types
+
+    public static LibraryItem Create(
+        ItemKind kind,
+        string title,
+        string author,
+        int copies = 1,
+        string section = "General",
+        string publisher = "Uknown")
+    {
+        // This method is going to use a swith to call the correct constructor
+        switch (kind)
+        {
+            case ItemKind.Book:
+                return new Book(title,author,copies);
+
+            case ItemKind.RerefenceBook:
+                return new ReferenceBook(title,author, section);
+            
+            case ItemKind.Magazine:
+                return new Magazine(title, author, copies, publisher);
+            
+            default: // No idea how you'd get here.
+                throw new LibraryException($"Uknown item kind: {kind}");
+
+        }
+    }
+}
